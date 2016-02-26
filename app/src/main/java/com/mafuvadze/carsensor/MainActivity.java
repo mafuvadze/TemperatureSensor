9d6da450bb0a6262613d4022e5a0f4839a870269
Anesu <mafuvadzeanesu@gmail.com>
2016-02-26 08:08:42 -0600

    improved UI

--- app/src/main/java/com/mafuvadze/carsensor/MainActivity.java
+++ app/src/main/java/com/mafuvadze/carsensor/MainActivity.java
@@ -1,6 +1,10 @@
 package com.mafuvadze.carsensor;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,12 +13,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements TempDisplay.OnFragmentInteractionListener, DeviceStateHistory.OnFragmentInteractionListener, Settings.OnFragmentInteractionListener {
-
-    ViewPager pager;
-    SlidingTabLayout tabs;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener
+{
+    Typeface roboto_thin, roboto_bold, roboto_italic;
+    TextView temp, name, battery, ulimit, llimit;
+    TextView bat_txt,ulimit_txt, llimit_txt, police_txt;
+    RelativeLayout popo;
+    CheckBox policeCB;
+    SeekBar llimitSeek, ulimitSeek;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +36,105 @@ public class MainActivity extends AppCompatActivity implements TempDisplay.OnFra
         setSupportActionBar(toolbar);
 
         initUi();
+        initListeners();
 
     }
 
+    private void initListeners()
+    {
+        popo.setOnClickListener(this);
+        ulimitSeek.setOnSeekBarChangeListener(this);
+        llimitSeek.setOnSeekBarChangeListener(this);
+    }
+
     private void initUi() {
-        pager = (ViewPager) findViewById(R.id.pager);
-        tabs = (SlidingTabLayout) findViewById(R.id.tab_host);
+        roboto_thin = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Thin.ttf");
+        roboto_bold = Typeface.createFromAsset(getAssets(),"fonts/RobotoCondensed-Bold.ttf");
+        roboto_italic = Typeface.createFromAsset(getAssets(),"fonts/Roboto-Italic.ttf");
 
-        Fragment[] tabList = new Fragment[]{TempDisplay.newInstance("1", "1"), DeviceStateHistory.newInstance("2", "2"), Settings.newInstance("3", "3")};
-        android.support.v4.view.PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), this, tabList);
-        pager.setAdapter(adapter);
+        temp = (TextView) findViewById(R.id.temp);
+        name = (TextView) findViewById(R.id.name);
+        battery = (TextView) findViewById(R.id.battery_txt);
+        ulimit = (TextView) findViewById(R.id.ulimit);
+        llimit = (TextView) findViewById(R.id.llimit);
+        police_txt = (TextView) findViewById(R.id.police_txt);
+        bat_txt = (TextView) findViewById(R.id.bat_txt);
+        ulimit_txt = (TextView) findViewById(R.id.ulimit_txt);
+        llimit_txt = (TextView) findViewById(R.id.llimit_txt);
+        popo = (RelativeLayout) findViewById(R.id.call_police);
+        llimitSeek = (SeekBar) findViewById(R.id.llimit_seek);
+        ulimitSeek = (SeekBar) findViewById(R.id.ulimit_seek);
 
-        tabs.setDistributeEvenly(true);
-        tabs.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-        tabs.setSelectedIndicatorColors(getResources().getColor(R.color.Red));
-        tabs.setCustomTabView(R.layout.custom_tab, R.id.tabText);
-        tabs.setViewPager(pager);
+
+        temp.setTypeface(roboto_thin);
+        name.setTypeface(roboto_bold);
+        ulimit.setTypeface(roboto_thin);
+        llimit.setTypeface(roboto_thin);
+        battery.setTypeface(roboto_thin);
+        police_txt.setTypeface(roboto_italic);
+        ulimit_txt.setTypeface(roboto_italic);
+        llimit_txt.setTypeface(roboto_italic);
+        bat_txt.setTypeface(roboto_italic);
+        ulimitSeek.setMax(120);
+        llimitSeek.setMax(120);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.call_police:
+                String msg = getString(R.string.call_police_info);
+                displayDialog(msg);
+        }
+    }
+
+    private void displayDialog(String msg)
+    {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage(msg)
+                .setTitle("Call Police")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(true)
+                .create();
+
+        dialog.show();
+
+    }
+
+    private void showToast(String msg, int duration)
+    {
+        Toast.makeText(this, msg, duration).show();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId())
+        {
+            case R.id.ulimit_seek:
+                ulimit.setText(Integer.toString(progress) + "°");
+                break;
+            case R.id.llimit_seek:
+                llimit.setText(Integer.toString(progress) + "°");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 
     @Override
@@ -55,16 +151,7 @@ public class MainActivity extends AppCompatActivity implements TempDisplay.OnFra
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
---
+++ app/src/main/java/com/mafuvadze/carsensor/Networks.java
@@ -0,0 +1,21 @@
+package com.mafuvadze.carsensor;
+
+import android.content.Context;
+
+/**
+ * Created by PLTW on 2/26/2016.
+ */
+public class Networks
+{
+    Context context;
+    public Networks(Context context)
+    {
+        this.context = context;
+
+    }
+
+    public void isBTConnected()
+    {
+
+    }
+}
