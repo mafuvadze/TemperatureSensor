0d44ce0d5ccd3dce716bff9bdb678807fa6b2967
Anesu <mafuvadzeanesu@gmail.com>
2016-02-26 17:32:03 -0600

    UI improvements, added BT listview

--- app/src/main/java/com/mafuvadze/carsensor/MainActivity.java
+++ app/src/main/java/com/mafuvadze/carsensor/MainActivity.java
@@ -3,7 +3,11 @@ package com.mafuvadze.carsensor;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,14 +15,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener
 {
@@ -28,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     RelativeLayout popo;
     CheckBox policeCB;
     SeekBar llimitSeek, ulimitSeek;
+    BluetoothAdapter btAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    public static final int BT_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initUi();
         initListeners();
-
+        initBT();
     }
 
     private void initListeners()
@@ -64,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         popo = (RelativeLayout) findViewById(R.id.call_police);
         llimitSeek = (SeekBar) findViewById(R.id.llimit_seek);
         ulimitSeek = (SeekBar) findViewById(R.id.ulimit_seek);
+        policeCB = (CheckBox) findViewById(R.id.call_police_CB);
 
 
         temp.setTypeface(roboto_thin);
@@ -77,6 +93,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bat_txt.setTypeface(roboto_italic);
         ulimitSeek.setMax(120);
         llimitSeek.setMax(120);
+        name.setText("no device connected");
+        temp.setText("--");
+        battery.setText("--%");
+        ulimitSeek.setProgress(85);
+        llimitSeek.setProgress(25);
 
     }
 
@@ -137,7 +158,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void initBT()
+    {
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(isBTConnected(btAdapter) && pairedDevices != null)
+        {
+            showDevices();
+        }
+        else
+        {
+            showToast("no bluetooth devices found", Toast.LENGTH_LONG);
+        }
+    }
+
+    private void showDevices()
+    {
+        Dialog dialog = new Dialog(this);
+        dialog.setTitle("Bluetooth devices nearby");
+        dialog.setContentView(R.layout.paired_devices);
+
+        ListView devices = (ListView) dialog.findViewById(R.id.devices);
+        devices.setAdapter(new PairListAdapter(this, R.layout.single_bt_row, pairedDevices));
+        dialog.show();
+    }
+
+    private boolean isBTConnected(BluetoothAdapter btAdapter)
+    {
+        if(btAdapter == null)
+        {
+            showToast("Bluetooth is not available", Toast.LENGTH_LONG);
+            return false;
+        }
+        else
+        {
+            if(!btAdapter.isEnabled())
+            {
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, BT_REQUEST);
+            }
+            else
+            {
+                pairedDevices = btAdapter.getBondedDevices();
+            }
+        }
+
+        return true;
+    }
+
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == BT_REQUEST && resultCode == RESULT_OK)
+        {
+            showToast("Bluetooth successfully enabled", Toast.LENGTH_LONG);
+            pairedDevices = btAdapter.getBondedDevices();
+        }
+        else
+        {
+            showToast("Bluetooth could not be enabled", Toast.LENGTH_LONG);
+        }
+    }
+
+    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -151,7 +235,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        if(id == R.id.BTdevices)
+        {
+            if(isBTConnected(btAdapter))
+            {
+                showDevices();
+            }
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    class PairListAdapter extends ArrayAdapter
+    {
+        List<BluetoothDevice> devices;
+        int resource;
+        public PairListAdapter(Context context, int resource, Set<BluetoothDevice> devices) {
+            super(context, resource);
+            this.resource = resource;
+
+            this.devices = new ArrayList<>();
+            for(BluetoothDevice device : devices)
+            {
+                this.devices.add(device);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return devices.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null)
+            {
+                LayoutInflater inflater = LayoutInflater.from(getBaseContext());
+                convertView = inflater.inflate(resource, parent, false);
+            }
+
+            BluetoothDevice device = devices.get(position);
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            TextView ip = (TextView) convertView.findViewById(R.id.ip);
+
+            name.setText(device.getName());
+            ip.setText(device.getAddress());
+
+            return convertView;
+        }
     }
 
 }
